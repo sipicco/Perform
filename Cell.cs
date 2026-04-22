65080480f8a504f250bac59641f1930148eafea3
sipicco <piccolo@omrt.tech>
2026-04-22 19:34:03 +0200

    Define starting cells

---
+++ Cell.cs
@@ -0,0 +1,44 @@
+﻿using Raylib_cs;
+
+namespace Perform
+{
+    internal class Cell
+    {
+        public int Xpos { get; set; }
+        public int Ypos { get; set; }
+        public int Xsize { get; set; }
+        public int Ysize { get; set; }
+        public CellAddress Address { get; set; }
+
+        public int? Value { get; set; }
+        public Color? CellColor { get; set; }
+
+        public Cell(int xPos, int yPos, int xsize, int ysize, CellAddress address, Color? cellColor)
+        {
+            Xpos = xPos;
+            Ypos = yPos;
+
+            Xsize = xsize;
+            Ysize = ysize;
+
+            Address = address;
+
+            CellColor = cellColor ?? Color.Brown;
+        }
+
+
+        public static void WriteInCell(Cell cell, string text, int textHeight, Color? textColor)
+        {
+            textColor = textColor ?? Color.Black;
+
+            var textWidth = Raylib.MeasureText(text, textHeight);
+
+            Raylib.DrawText(text,
+                cell.Xpos / 2 - textWidth / 2,
+                cell.Ypos / 2 - textHeight / 2,
+                textHeight, Color.Green);
+        }
+    }
+
+    internal record CellAddress(string ColumnId, string RowId);
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Perform;
 using Raylib_cs;
 
 class Program
@@ -18,27 +19,75 @@ class Program
         {
             var frameTimeMicroseconds = Raylib.GetFrameTime() / 1e6;
             var fps = Raylib.GetFPS();
-            var height = Raylib.GetRenderHeight();
-            var width = Raylib.GetRenderWidth();
+            var totHeight = Raylib.GetRenderHeight();
+            var totWidth = Raylib.GetRenderWidth();
 
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Color.RayWhite);
 
-            Raylib.DrawRectangle(1, 1, width: width - 2, height - 2, Color.Blue);
+            // Draw background
+            Raylib.DrawRectangle(1, 1, width: totWidth - 2, totHeight - 2, Color.Blue);
 
+            // Generate cells with their address
+            var cells = GenerateCells(totHeight, totWidth);
 
-            Raylib.DrawRectangle(width / 2 - 100, height / 2 - 75, width: 200, 150, Color.Red);
-
+            // Draw cells with value
+            DrawCells(cells);
 
             var textHeight = 20;
             var text = "42";
             var textWidth = Raylib.MeasureText(text, textHeight);
 
-            Raylib.DrawText(text, width / 2 - textWidth / 2, height / 2 - textHeight / 2, textHeight, Color.Green);
-            Raylib.DrawText($"FPS {fps};\nMicroseconds per frame: {frameTimeMicroseconds:F3}", 20, 20, 20, Color.White);
+            Raylib.DrawText(text, totWidth / 2 - textWidth / 2, totHeight / 2 - textHeight / 2, textHeight, Color.Green);
+            Raylib.DrawText($"FPS {fps};\nMicroseconds per frame: " +
+                $"{frameTimeMicroseconds:F3}", 20, 20, 20, Color.White);
             Raylib.EndDrawing();
         }
 
         Raylib.CloseWindow();
+    }
+
+    private static void DrawCells(Cell[] cells)
+    {
+        foreach (var c in cells)
+        {
+            // Draw cell geometry
+            Raylib.DrawRectangle(c.Xpos, c.Ypos, c.Xsize, c.Ysize, (Color)c.CellColor!);
+
+            // Draw cell avalue
+        }
+    }
+
+    private static Cell[] GenerateCells(int totHeight, int totWidth)
+    {
+        var widthSpan = totWidth / 3;
+        var heightSpan = totHeight / 3;
+
+        var cellWidth = widthSpan / 2;
+        var cellHeight = heightSpan / 2;
+
+        Cell[] cells = new Cell[9];
+        for (int i = 0; i < cells.Length; i++)
+        {
+            var x = widthSpan * (i % 3);
+            var y = heightSpan * (i / 3);
+
+            // offset half span
+            x = x + widthSpan / 2;
+            y = y + heightSpan / 2;
+
+            // center cells
+            x = x - cellWidth / 2;
+            y = y - cellHeight / 2;
+
+            char[] columnLetters = { 'A', 'B', 'C' };
+
+            CellAddress address = new CellAddress($"{columnLetters[i % 3]}", $"{i / 3 + 1}");
+
+            cells[i] = new Cell(x, y, widthSpan / 2, heightSpan / 2, address, null);
+
+        }
+
+        return cells;
     }
 }
